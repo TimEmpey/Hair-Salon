6d264da9e51141a52a683fd263cb23c00e3fd3d4
Tim Empey <timothyempey@gmail.com>
2022-10-07 10:53:31 -0700

    fix file swap issue

---
+++ HairSalon/Models/Stylists.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public Cuisine()
+    {
+      this.Clients = new HashSet<Client>();
+    }
+
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public string Hired { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+  }
+}
